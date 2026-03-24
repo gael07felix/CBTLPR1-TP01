8ae0be32cd28e689845646b66d950b42862186e1
gael07felix <gael.biro@gmail.com>
2026-03-24 17:20:02 -0300

    Corrigindo cálculo

--- src/TP01Ex16.java
+++ src/TP01Ex16.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.lang.Math;
+
 /*
  * Exercício 16
  * Enunciado: Entrar via teclado com o valor de um ângulo em graus, calcular e exibir as
@@ -16,20 +17,22 @@ public class TP01Ex16 {
 
         System.out.print("Digite o ângulo em graus: ");
         double graus = scanner.nextDouble();
+
         double radianos = Math.toRadians(graus);
         double seno = Math.sin(radianos);
         double cosseno = Math.cos(radianos);
         double tangente = Math.tan(radianos);
         double secante;
+
         if (cosseno != 0) {
             secante = 1 / cosseno;
         } else {
             secante = Double.NaN;
+        } 
 
         System.out.println("Seno: " + seno);
         System.out.println("Cosseno: " + cosseno);
         System.out.println("Tangente: " + tangente);
         System.out.println("Secante: " + secante);
-        }
     }
 }
